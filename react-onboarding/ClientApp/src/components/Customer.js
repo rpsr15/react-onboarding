2fa334cd76d48f0031fd1c1d498fcc5de8ab482e
Ravi Rathore <ravirathore15@icloud.com>
2020-02-20 02:33:59 +1100

    added sematic ui package

---
+++ react-onboarding/ClientApp/src/components/Customer.js
@@ -0,0 +1,21 @@
+﻿import React, { Component } from 'react';
+
+
+export class Customer extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            customers: [],
+            loading: true
+        }
+    }
+
+    render() {
+        return (
+            <p>here is list of customer</p>
+            );
+    }
+
+}
